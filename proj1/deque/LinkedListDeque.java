afc1402de832e913e285e73a517beb63b5f44f98
Wang Xinran <wang13373056911@163.com>
2025-03-18 20:08:15 +0800

    0

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,6 +1,12 @@
 package deque;
+import java.util.Iterator;
 
-public class LinkedListDeque<witem> {
+import static edu.princeton.cs.algs4.StdOut.print;
+
+public class LinkedListDeque<witem> implements Iterable<witem> {
+
+    //node and init
+
     private class Node {
         witem data;
         Node next;
@@ -14,16 +20,45 @@ public class LinkedListDeque<witem> {
     private Node heail;
     private int size;
     public LinkedListDeque() {
-        heail = new Node(null, heail, heail);
+        heail = new Node(null, null, null);
+        heail.next = heail.prev = heail;
         size = 0;
     }
+
+
+    //iterator implement
+
+    public class Listiterator<witem> implements Iterator<witem> {
+
+        private Node current;
+        public Listiterator() {
+            current = heail.next;
+        }
+        @Override
+        public boolean hasNext() {
+            return current != heail;
+        }
+
+        @Override
+        public witem next() {
+            witem cur_data= (witem) current.data;
+            current=current.next;
+            return cur_data;
+        }
+    }
+    public Listiterator<witem> iterator(){
+        return new Listiterator<>();
+    }
+
     public void addFirst(witem d) {
         Node newNode = new Node(d, heail.next, heail);
         heail.next = newNode;
         newNode.next.prev = newNode;
+        size++;
         return;
     }
     public witem removeFirst(){
+        if (size == 0) return null;
         witem data = heail.next.data;
         heail.next=heail.next.next;
         heail.next.prev=heail;
@@ -33,22 +68,101 @@ public class LinkedListDeque<witem> {
     public void addLast(witem d) {
         Node newNode = new Node(d, heail, heail.prev);
         newNode.prev.next = newNode;
-        newNode.prev.prev = newNode;
+        newNode.next.prev = newNode;
+        size++;
         return;
     }
+    public witem removeLast(){
+        if (size == 0) return null;
+        witem data = heail.prev.data;
+        heail.prev.prev.next = heail;
+        heail.prev=heail.prev.prev;
+        size--;
+        return data;
+    }
+
+    //about size
+
     public boolean isEmpty(){
         return size == 0;
     }
     public int size(){
         return size;
     }
-    public void print(){
+
+    //about output
+
+    @Override
+    public String toString() {
+        Listiterator iter = (Listiterator) iterator();
+        StringBuilder base=new StringBuilder();
+        base.append("[");
+        for(witem i:this)
+        {
+            base.append(i.toString());
+            base.append(" ");
+        }
+        base.append("]");
+        return base.toString();
+    }
+
+    public witem get(int index){
+        if(index+1>size())return null;//判断是否超出索引范围
+        Listiterator iter =  iterator();
+        while(iter.hasNext() && (index>0)) {
+            iter.next();
+            index--;
+        }
+        return (witem) iter.next();
+    }
+    public witem getRecursive(int index){
+        return recursive(index,heail.next);
+    }
+
+    public void printDeque(){
         Node current = heail.next;
         while(current.data != null){
             System.out.print(current.data + " ");
+            current = current.next;
         }
         System.out.println();
         return;
     }
+    public static void main(String[] args) {
+        LinkedListDeque <Integer> deque = new LinkedListDeque();
+        for (int i = 5; i < 10; i++) {
+            deque.addLast(i);
+        }
+        for (int i = 4; i > 0; i--) {
+            deque.addFirst(i);
+        }
+        System.out.println(deque);
+    }
+    //none
+    @Override
+    public boolean equals(Object o){
+        if(o instanceof LinkedListDeque ohther){
+            if(ohther.size() != size){
+                return false;
+            }
+            Listiterator oitr = ohther.iterator();
+            Listiterator titr=this.iterator();
+            while(titr.hasNext()){
+                if(!(oitr.next().equals(titr.next()))) return false;
+                titr.next();
+                oitr.next();
+            }
+            return true;
+        }
+        return false;
+    }
+    //helper method
+    private witem recursive(int index,Node cur){
+        if(cur==heail && index==0) return null;//防止索引超范围
+        if (index == 0) return cur.data;
+        cur = cur.next;
+        return recursive(index-1, cur);
+    }
+
 
 }
--- proj1/deque/LinkedListDequeTest.java
+++ proj1/deque/LinkedListDequeTest.java
@@ -15,7 +15,7 @@ public class LinkedListDequeTest {
     public void addIsEmptySizeTest() {
 
         System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
+
         LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
 		assertTrue("A newly initialized LLDeque should be empty", lld1.isEmpty());
@@ -34,7 +34,7 @@ public class LinkedListDequeTest {
 
 		System.out.println("Printing out deque: ");
 		lld1.printDeque();
-		*/
+
     }
 
     @Test
@@ -42,7 +42,7 @@ public class LinkedListDequeTest {
     public void addRemoveTest() {
 
         System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
+
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 		// should be empty
 		assertTrue("lld1 should be empty upon initialization", lld1.isEmpty());
@@ -54,7 +54,7 @@ public class LinkedListDequeTest {
 		lld1.removeFirst();
 		// should be empty
 		assertTrue("lld1 should be empty after removal", lld1.isEmpty());
-        */
+
     }
 
     @Test
@@ -62,7 +62,7 @@ public class LinkedListDequeTest {
     public void removeEmptyTest() {
 
         System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
+
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<>();
         lld1.addFirst(3);
 
@@ -77,14 +77,14 @@ public class LinkedListDequeTest {
         errorMsg += "  actual size() returned 0\n";
 
         assertEquals(errorMsg, 0, size);
-        */
+
     }
 
     @Test
     /* Check if you can create LinkedListDeques with different parameterized types*/
     public void multipleParamTest() {
 
-        /*
+
         LinkedListDeque<String>  lld1 = new LinkedListDeque<String>();
         LinkedListDeque<Double>  lld2 = new LinkedListDeque<Double>();
         LinkedListDeque<Boolean> lld3 = new LinkedListDeque<Boolean>();
@@ -96,7 +96,7 @@ public class LinkedListDequeTest {
         String s = lld1.removeFirst();
         double d = lld2.removeFirst();
         boolean b = lld3.removeFirst();
-        */
+
     }
 
     @Test
@@ -104,7 +104,7 @@ public class LinkedListDequeTest {
     public void emptyNullReturnTest() {
 
         System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
+
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 
         boolean passed1 = false;
@@ -112,7 +112,7 @@ public class LinkedListDequeTest {
         assertEquals("Should return null when removeFirst is called on an empty Deque,", null, lld1.removeFirst());
         assertEquals("Should return null when removeLast is called on an empty Deque,", null, lld1.removeLast());
 
-        */
+
     }
 
     @Test
@@ -120,7 +120,7 @@ public class LinkedListDequeTest {
     public void bigLLDequeTest() {
 
         System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
+
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
         for (int i = 0; i < 1000000; i++) {
             lld1.addLast(i);
@@ -134,7 +134,7 @@ public class LinkedListDequeTest {
             assertEquals("Should have the same value", i, (double) lld1.removeLast(), 0.0);
         }
 
-        */
+
     }
     @Test
     public void btest_node_add_empty_size() {
@@ -142,10 +142,54 @@ public class LinkedListDequeTest {
         for (int i = 5; i < 10; i++) {
             deque.addLast(i);
         }
-        for (int i = 5; i > 0; i--) {
+        for (int i = 4; i > 0; i--) {
             deque.addFirst(i);
         }
-        assertEquals(deque.size(), 9);
-
+        String expects="[1 2 3 4 5 6 7 8 9 ]";
+        assertEquals(9, deque.size());
+        assertEquals(expects, deque.toString());
     }
+    @Test
+    public void test_get_1st() {
+        LinkedListDeque <Integer> deque = new LinkedListDeque();
+        for (int i = 5; i < 10; i++) {
+            deque.addLast(i);
+        }
+        for (int i = 4; i > 0; i--) {
+            deque.addFirst(i);
+        }
+        String expects="7";
+        assertEquals(expects, deque.get(6).toString());
+    }
+    @Test
+    public void test_get_2nd() {
+        LinkedListDeque <Integer> deque = new LinkedListDeque();
+        for (int i = 5; i < 10; i++) {
+            deque.addLast(i);
+        }
+        for (int i = 4; i > 0; i--) {
+            deque.addFirst(i);
+        }
+        String expects="1";
+        assertEquals(expects, deque.get(0).toString());
+        assertEquals(expects, deque.getRecursive(0).toString());
+    }
+    @Test
+    public void test_get_3rd() {
+        LinkedListDeque <Integer> deque = new LinkedListDeque();
+        for (int i = 5; i < 10; i++) {
+            deque.addLast(i);
+        }
+        for (int i = 4; i > 0; i--) {
+            deque.addFirst(i);
+        }
+        String expects="9";
+        String expectss="9";
+        System.out.println(deque);
+        assertEquals(expects, deque.get(8).toString());
+        assertEquals(expectss, deque.getRecursive(8).toString());
+    }
+    @Test
+    public void emptyLinkedListTest() {}
+
 }
